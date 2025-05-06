34d9362197a0ba1b9a8888b169d098cb0e91e129
xt0623 <3341685238@qq.com>
2025-05-06 15:16:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -49,11 +49,11 @@ noButton.addEventListener("click", function () {
   }
 
   // 图片变化（前 5 次变化）
-  if (clickCount === 1) mainImage.src = "images/shocked.png"; // 震惊
-  if (clickCount === 2) mainImage.src = "images/think.png"; // 思考
-  if (clickCount === 3) mainImage.src = "images/angry.png"; // 生气
-  if (clickCount === 4) mainImage.src = "images/crying.png"; // 哭
-  if (clickCount >= 5) mainImage.src = "images/crying.png"; // 之后一直是哭
+  if (clickCount === 1) mainImage.src = "images/IMG_0160.jpeg"; // 震惊
+  if (clickCount === 2) mainImage.src = "images/IMG_0160.jpeg"; // 思考
+  if (clickCount === 3) mainImage.src = "images/IMG_0160.jpeg"; // 生气
+  if (clickCount === 4) mainImage.src = "images/IMG_0160.jpeg"; // 哭
+  if (clickCount >= 5) mainImage.src = "images/IMG_0160.jpeg"; // 之后一直是哭
 });
 
 // Yes 按钮点击后，进入表白成功页面
